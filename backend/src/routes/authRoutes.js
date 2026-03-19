c9960f441e9813618ca400cf43b13bd20d11d3c3
Hygge194 <Huyen.luungoc194@hcmut.edu.vn>
2026-03-19 23:26:18 +0700

    feat: add auth middleware

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,16 @@
-console.log("👉 MÁY DÒ: Đã chạy thành công vào file authRoutes.js"); // Thêm dòng này
-
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+const {verifyToken} = require('../middleware/authMiddleware');
+
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
+router.get('/profile', verifyToken, (req, res) => {
+    res.status(200).json({
+        message: 'Xác thực thành công!',
+        user: req.user 
+    });
+});
 module.exports = router;
